2c87a32057fdc1d5fb0b1d0257e39f8db9d526f5
Andrew Liu <317369453@qq.com>
2019-12-06 18:34:11 +0800

    2019-12-06 18:34:05

---
+++ Golang/goroutine_panic.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+import "strconv"
+
+import "time"
+
+func main() {
+	ch := make(chan int)
+	go tel(ch)
+	setCloseTimer(ch, 1e9)
+	for i := range ch {
+		fmt.Println(strconv.Itoa(i))
+	}
+
+}
+
+func setCloseTimer(ch chan int, i time.Duration) {
+	go func() {
+		time.Sleep(i)
+		close(ch)
+	}()
+}
+
+func tel(ch chan int) {
+	defer func() {
+		if _, ok := recover().(error); ok {
+			fmt.Println("channel is closed")
+		}
+	}()
+	i := 1
+	for {
+		ch <- i
+		i++
+	}
+}
