712f6dd1a1c6c0621327509caa96d244adb80734
Andrew Liu <317369453@qq.com>
2019-12-25 17:27:58 +0800

    2019-12-25 17:27:56

---
+++ Golang/general_lazy_evaluation2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+var resume chan int
+
+func integers() chan int {
+	yield := make(chan int)
+	num1 := 0
+	num2 := 1
+	go func() {
+		yield <- 0
+		for {
+			num3 := num1 + num2
+			yield <- num3
+			num1 = num2
+			num2 = num3
+		}
+	}()
+	return yield
+}
+
+func generateInteger() int {
+	return <-resume
+}
+
+func main() {
+	resume = integers()
+	for index := 0; index < 10; index++ {
+		fmt.Println(generateInteger())
+	}
+}
