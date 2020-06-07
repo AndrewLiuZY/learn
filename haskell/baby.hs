78e74a109ad7e845e484faf7d937354afa7d2b68
AndrewLiu <48308664+AndrewLiuZY@users.noreply.github.com>
2020-06-07 23:19:36 +0800

    add

--- haskell/baby.hs
+++ haskell/baby.hs
@@ -107,3 +107,44 @@ quickSort [] = []
 quickSort (x:xs) = smallerSorted ++ [x] ++ biggerSorted
     where smallerSorted = quickSort [a|a<-xs,a<=x]
           biggerSorted = quickSort [a|a<-xs,a>x]
+
+applyTwice :: (a->a)->a->a
+applyTwice f x = f (f x)
+
+zipWith' :: (a->b->c)->[a]->[b]->[c]
+zipWith' _ _ [] = []
+zipWith' _ [] _ = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a->b->c)->b->a->c
+flip' f x y = f y x 
+
+map' ::(a->b)->[a]->[b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a->Bool)->[a]->[a]
+filter' _ [] = []
+filter' f (x:xs)
+    | f x = x:filter' f xs
+    | otherwise = filter' f xs
+
+largestDivisible :: (Integral a) => a  
+largestDivisible = head (filter p [100000,99999..])  
+    where p x = x `mod` 3829 == 0
+
+chain ::(Integral a)=>a->[a]
+chain 1 =[1]
+chain n
+    | even n = n:chain (div n 2)
+    | odd n = n:chain (n*3+1) 
+
+elem'foldl ::(Eq a)=>a->[a]->Bool
+elem'foldl a xs = foldl (\acc x->if a==x then True else acc) False xs
+
+map'foldr ::(a->b)->[a]->[b]
+map'foldr f xs = foldr (\x acc->(f x):acc) [] xs
+
+map'foldl ::(a->b)->[a]->[b]
+map'foldl f xs = foldl (\acc x->(f x):acc) [] xs
+
